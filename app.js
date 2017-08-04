af57e2e1065edbb2f26424a746207f6d5673111b
Mitchel Severe <truth531@gmail.com>
2017-08-04 18:45:25 -0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const logger = require('morgan');
+const path = require('path');
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const passport = require('passport');;
+const favicon = require('serve-favicon');
+
+var app = express();
+require('dotenv').config();
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(methodOverride('_method'));
+app.use(cookieParser());
+app.use(session({
+  secret: process.env.SECRET_KEY,
+  resave: false,
+  saveUninitialized: true,
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
+
+const indexRoute = require('./routes/index-route');
+app.use('/', indexRoute);
+const songRoutes = require('./routes/user-routes');
+app.use('/songs', songRoutes);
+const authRoutes = require('./routes/auth-routes');
+app.use('/auth', authRoutes);
+const userRoutes = require('./routes/user-routes');
+app.use('/user', userRoutes);
+
+app.get('*', (req, res) => {
+    res.status(404).send('not found!');
+});
