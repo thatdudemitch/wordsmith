54090773f2af6802d75148a403cdc4cb693624cf
mitchelsevere <mitchelsevere@gmail.com>
2019-11-12 10:52:22 -0500

    heroku deployment

--- server.js
+++ server.js
@@ -19,23 +19,21 @@ app.use(cookieParser());
 app.use(session({secret: process.env.SECRET_KEY, resave: false, saveUninitialized: true}));
 app.use(passport.initialize());
 app.use(passport.session());
-app.use(express.static(path.join(__dirname, '/../client/build')));
+
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html')); 
+    });
+}
 
 app.listen(port, () => {
     console.log(`Currently listening on port ${port}`);
 });
 // routes
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname + '/../client/build/index.html'));
-});
-
 const songsRouter = require('./routes/songs');
 const usersRouter = require('./routes/users');
 const authRouter = require('./routes/auth');
 app.use('/', authRouter);
 app.use('/songs', songsRouter);
 app.use('/profile', usersRouter);
-
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/../client/build/index.html'));
-});
